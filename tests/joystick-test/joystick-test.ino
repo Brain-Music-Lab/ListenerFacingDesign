4f34908bb0820ba40e9e6013676b783caf7d3f5b
smehdizadeh <sophia.mehdizadeh@colorado.edu>
2025-03-10 12:16:42 -0600

    finish troubleshooting joystick

--- tests/joystick-test/joystick-test.ino
+++ tests/joystick-test/joystick-test.ino
@@ -1,3 +1,6 @@
+//NOTE: IT IS BEST TO INCLUDE A LOW PASS FILTER ON EACH INPUT PIN
+// 1M RESISTOR AND 47nF CAPACITOR WORKS WELL.
+
 int jstk_input_up_pin = A0;
 int jstk_input_down_pin = A1;
 int jstk_input_right_pin = A2;
@@ -20,28 +23,24 @@ void loop() {
 
 void read_input_direction() {
   //prints the direction user moves joystick
+  //open in serial PLOTTER
 
   int up = analogRead(jstk_input_up_pin);
   int down = analogRead(jstk_input_down_pin);
   int left = analogRead(jstk_input_left_pin);
   int right = analogRead(jstk_input_right_pin);
 
-  //check diagonals first
-  if((up == 0) && (left == 0)) {
-    Serial.println("UP-LEFT");
-  }
-  else if((up == 0) && (right == 0)) {
-    Serial.println("UP-RIGHT");
-  }
-  else if((down == 0) && (right == 0)) {
-    Serial.println("DOWN-RIGHT");
-  }
-  else if((down == 0) && (left == 0)) {
-    Serial.println("DOWN-LEFT");
-  }
-  else if((down == 0)) {
-    Serial.println("DOWN");
-  }
+  Serial.print("UP:");
+  Serial.print(up);
+  Serial.print(",");
+  Serial.print("DOWN:");
+  Serial.print(down);
+  Serial.print(",");
+  Serial.print("LEFT:");
+  Serial.print(left);
+  Serial.print(",");
+  Serial.print("RIGHT:");
+  Serial.println(right);
 
-  delay(10);
+  delay(50);
 }
