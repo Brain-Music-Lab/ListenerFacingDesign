0f8bae4c0a2a95dcb12a8a57ac0954a309a694a2
Daniel Ethridge <ethridge.daniel.m@gmail.com>
2025-04-30 16:17:21 -0600

    Updates

--- backend/joystick.py
+++ backend/joystick.py
@@ -47,10 +47,10 @@ try:
         # For buttons
         for buttons, lights in zip(button_press_pins.items(),
                                    button_light_pins.items()):
-            if GPIO.input(buttons[1]) == GPIO.HIGH and not green_on:
+            if GPIO.input(buttons[1]) == GPIO.LOW and not green_on:
                 GPIO.output(lights[1], GPIO.HIGH)
                 green_on = True
-            elif GPIO.input(buttons[1]) == GPIO.LOW and green_on:
+            elif GPIO.input(buttons[1]) == GPIO.HIGH and green_on:
                 GPIO.output(lights[1], GPIO.LOW)
                 green_on = False
                 # print(f"{direction} pressed")
